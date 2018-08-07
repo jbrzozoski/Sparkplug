f2683b1a2b34cd69a83deafdde2cb6877c1890f7
wes-johnson <wes0johnson@gmail.com>
2018-08-07 10:33:05 -0700

    More copyright fixes

--- client_libraries/c/test.sh
+++ client_libraries/c/test.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+#/********************************************************************************
+# * Copyright (c) 2014, 2018 Cirrus Link Solutions and others
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * SPDX-License-Identifier: EPL-2.0
+# *
+# * Contributors:
+# *   Cirrus Link Solutions - initial implementation
+# ********************************************************************************/
+
 #echo "Running static example..."
 #./test/test_static
 
--- client_libraries/python/sparkplug_b.py
+++ client_libraries/python/sparkplug_b.py
@@ -1,3 +1,15 @@
+#/********************************************************************************
+# * Copyright (c) 2014, 2018 Cirrus Link Solutions and others
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * SPDX-License-Identifier: EPL-2.0
+# *
+# * Contributors:
+# *   Cirrus Link Solutions - initial implementation
+# ********************************************************************************/
 import sparkplug_b_pb2
 import time
 from sparkplug_b_pb2 import Payload
